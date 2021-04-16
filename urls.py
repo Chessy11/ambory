3b9d84d5f78e3b4c327b19887fee712993a6d4d2
Chessy11 <78972188+Chessy11@users.noreply.github.com>
2021-04-16 13:34:01 +0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import (
-        cart_home, 
-        cart_update, 
-        )
+from .views import simpleCheckout
 
-urlpatterns = [
-    path('', cart_home, name='home'),
-    path('update/', cart_update, name='update'),
+urlpatterns = [ 
+    path('', simpleCheckout, name='checkout'),
+    path('simple-checkout/', simpleCheckout, name='simple-checkout'),
+
 ]
 
-app_name = 'carts'
+app_name = 'checkout'
